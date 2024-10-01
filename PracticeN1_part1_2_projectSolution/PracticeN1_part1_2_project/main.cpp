d234c9deca9d91623f488d4e89e32bf5087560ea
Platon Martynov <50545147+Userteck@users.noreply.github.com>
2024-10-01 10:08:54 +0300

    Add files via upload

---
+++ PracticeN1_part1_2_projectSolution/PracticeN1_part1_2_project/main.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream> // include header file iostream that contains declarations of 
+// standard library elements for input/output 
+using namespace std; // statement that allows to use names defined under std 
+// namespace in iostream header file. Names like cin и cout 
+// become available without having to put a prefix std:: 
+// before them 
+int main() // program main function that program execution starts with
+{ // beginning of the block
+	int a, b, res, res1, res2, res3, res4; // declaration for variable а of type int
+
+	cout << "Enter number: "; // text "Enter number: " output 
+	cin >> a; // waiting for user to enter integer number. Number then is 
+	// stored in variable а
+	cout << "Enter number: ";
+	cin >> b; 
+
+	res = a + b; // calculate square of variable a value. Result is then stored in 
+	cout << endl << "Number " << a << " + " << b << " = " << res << endl;
+
+	res1 = a - b; // calculate square of variable a value. Result is then stored in 
+	cout << endl << "Number " << a << " - " << b << " = " << res1 << endl;
+
+	res2 = a * b; // calculate square of variable a value. Result is then stored in 
+	cout << endl << "Number " << a << " * " << b << " = " << res2 << endl;
+
+	res3 = a / b; // calculate square of variable a value. Result is then stored in 
+	cout << endl << "Number " << a << " / " << b << " = " << res3 << endl;
+
+	
+
+	return 0; // main function returns 0 and this is interpreted as successful 
+	// program termination 
+} // end of the block
---
+++ projectSolution/project/main.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h> // include header file stdio.h that contains declarations for 
+// scanf and printf functions 
+int main() // program main function that program execution starts with
+{ // beginning of the block 
+	int a, b, res, res1, res2, res3, res4; // declaration for variable а of type int
+	float k, m;
+
+	//int res; // declaration for variable res of type int 
+
+	printf("Enter first number: "); // text "Enter number: " output
+	scanf_s("%i", &a); // waiting for user to enter integer number. Number then is 
+	// stored in variable а
+	printf("Enter second number: "); // text "Enter number: " output
+	scanf_s("%i", &b); // waiting for user to enter integer number. Number then is 
+	res = a + b; // calculate square of variable a value. Result is then stored in 
+	// variable res 
+	printf("%i + %i is %i\n", a, b, res); // output the result
+	res1 = a - b; // calculate square of variable a value. Result is then stored in 
+	printf("%i - %i is %i\n", a, b, res1); // output the result
+	res2 = a * b; // calculate square of variable a value. Result is then stored in 
+	printf("%i * %i is %i\n", a, b, res2); // output the result
+	res3 = a / b; // calculate square of variable a value. Result is then stored in 
+	printf("%i / %i is %i\n", a, b, res3); // output the result
+	return 0; // main function returns 0 and this is interpreted as successful 
+	// program termination 
+} // end of the block
